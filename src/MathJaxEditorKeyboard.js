bbbce0bd24c2582fd9e28b75b78bf79f701be61b
Ian Lucas <ianlucas_ferreira@hotmail.com>
2017-02-27 02:21:07 -0300

    1.2.0

--- src/MathJaxEditorKeyboard.js
+++ src/MathJaxEditorKeyboard.js
@@ -13,7 +13,7 @@ class MathJaxEditorKeyboard {
     const core = new Core(options);
 
     this.mathjaxEditor = core.mathjaxEditor;
-    this.version = '1.1.6';
+    this.version = '1.2.0';
   }
 
   /**
